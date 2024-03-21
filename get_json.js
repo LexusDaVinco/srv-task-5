9d1498037ddd2c89803e5aa847033f465af00aa9
AlexiusDeVinco <119763746+AlexiusDeVinco@users.noreply.github.com>
2024-03-21 23:03:17 +0100

    new bucket

--- get_json.js
+++ get_json.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3()
 
 const getJSON = async ()=> { 
   await s3.getObject({
-  Bucket: "defiant-school-uniform-clamCyclicDB",
+  Bucket: "cyclic-defiant-school-uniform-clam-eu-north-1",
   Key: "jsonData.json",
 }).promise()
 }
